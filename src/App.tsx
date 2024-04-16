8f6b7a305936660ab21264cfd65847d73ef1890e
mihirgandecha <mihirgandecha@outlook.com>
2024-04-16 18:35:18 +0100

    Adding bootstrap to new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Message from "./Message"
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return <div><Message></Message></div>
 }
 
-export default App;
+export default App; 
