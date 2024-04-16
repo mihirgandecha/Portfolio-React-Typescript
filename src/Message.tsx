2ee7202c17d5a70012fc9fe66eea2b7b6593b5c1
mihirgandecha <mihirgandecha@outlook.com>
2024-04-16 16:39:23 +0100

    Introduce conditions using constants: Understand how UI can be described via Syntax

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,11 @@ function Message() {
     //JSX: .js XML
     const First_Name = 'Mihir';
     const Last_Name = 'Gandecha';
-    return <h1>{First_Name} {Last_Name}</h1>
+    //why JSX is so good! can describe the UI using syntax!
+    if(First_Name && Last_Name){{First_Name} {Last_Name}
+        return <h1>{First_Name} {Last_Name}</h1>
+    }
+    return <h1>Mihir G</h1>
 }
 
 export default Message;
